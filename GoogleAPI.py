661f3b196638b487cd3aafa1fc8be5c5cb047cce
Yossi1302 <yossitz1302@gmail.com>
2021-11-08 12:01:58 +0200

    fixing google api

--- GoogleAPI.py
+++ GoogleAPI.py
@@ -1,13 +1,13 @@
-from apiclient.discovery import build
 from googleapiclient.discovery import build
 import json
 
 my_api_key = "AIzaSyCFZWHPLT3Jjz8plycCdJIpD1ONu4tIqWE"
 my_cse_id = '2c7acc6bae33119b6'
 
-def ExecuteSearch(topic,website):
 
-    search = topic +" - "+website
+def ExecuteSearch(topic, website):
+
+    search = topic + " - "+website
 
     resourse = build("customsearch", 'v1', developerKey=my_api_key).cse()
     result = resourse.list(q=search, cx=my_cse_id).execute()
